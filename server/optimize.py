fa5a4fb941595800bb1d4490cd30b87ded1146c9
Jonas Bossking123 <jonasmalm@outlook.com>
2020-07-18 02:58:55 +0200

    Kodade lite salongs - alla BV + målfunktion klara. MF spottar dock ut z = -0.0??

--- server/optimize.py
+++ server/optimize.py
@@ -30,8 +30,27 @@ def recieve_data():
     
     pref_matrix = create_pref_matrix(juniors)
     
-    print(variables)
-    print(pref_matrix)
+    create_std_constraints(solver, variables, boat_parameters, juniors, pref_matrix)
+    create_custom_constraints(solver, variables, constraints, juniors, boat_parameters)
+    
+    objective = solver.Objective()
+    # In Python, you can also set the objective as follows.
+    # obj_expr = [data['obj_coeffs'][j] * x[j] for j in range(data['num_vars'])]
+    # solver.Maximize(solver.Sum(obj_expr))
+    
+    object_expression = [pref_matrix[i][j] * variables['y'][i, j, b] 
+                         for i in range(len(juniors)) for j in range(len(juniors)) for b in range(boat_parameters['noBoats']) if j != i]
+    solver.Maximize(variables['worst_boat'] + solver.Sum(object_expression))
+    
+    #maximize z : v + sum{i in JUNIORER, j in JUNIORER, b in BATAR} p[i,j]*y[i,j,b];
+    
+    
+    status = solver.Solve()
+    if status == pywraplp.Solver.OPTIMAL:
+        print('Objective value =', solver.Objective().Value())
+        
+    else:
+        print('The problem does not have an optimal solution.')
     
     return 'Ok', 200
 
@@ -83,11 +102,59 @@ def create_pref_matrix(juniors):
     
     return p
     
-def create_std_constraints(solver, variables):
+def create_std_constraints(solver, variables, boat_parameters, juniors, pref_matrix):
     #En junior i varje båt
-    for i in range(juniors):
-        constraint_exp = variables['x'][i][b] for b in no_boats
-        solver.Add(sum(constraint_exp) = 1)
+    for i in range(len(juniors)):
+        constraint_exp = [variables['x'][i, b] for b in range(boat_parameters['noBoats'])]
+        solver.Add(solver.Sum(constraint_exp) == 1)
+        
+    #min capacity
+    for b in range(boat_parameters['noBoats']):
+        constraint_exp = [variables['x'][i, b] for i in range(len(juniors))]
+        solver.Add(solver.Sum(constraint_exp) >= boat_parameters['minCrew'] * variables['boat_used'][b])
+    
+    #max capacity
+    for b in range(boat_parameters['noBoats']):
+        constraint_exp = [variables['x'][i, b] for i in range(len(juniors))]
+        solver.Add(solver.Sum(constraint_exp) <= boat_parameters['maxCrew'] * variables['boat_used'][b])
+        
+    #Koppling x till y (jun i med jun j) -->
+    for i in range(len(juniors)):
+        for j in range(len(juniors)):
+            for b in range(boat_parameters['noBoats']):
+                if i != j:
+                    solver.Add(2 * variables['y'][i, j, b] <= variables['x'][i, b] + variables['x'][j, b])
+    
+    #Sämsta båten --> målfunktion
+    for b in range(boat_parameters['noBoats']):
+        constraint_exp = [pref_matrix[i][j] * variables['y'][i, j, b] for i in range(len(juniors)) for j in range(len(juniors)) if j!= i] 
+        solver.Add(variables['worst_boat'] <= solver.Sum(constraint_exp))
+        
+        
+def create_custom_constraints(solver, variables, constraints, juniors, boat_parameters):
+    for c in constraints:
+        name1 = c['name1']
+        name2 = c['name2']
+        
+        i = list(filter(lambda j: juniors[j]['name'] == name1, range(len(juniors))))[0]
+        j = list(filter(lambda j: juniors[j]['name'] == name2, range(len(juniors))))[0]
+        
+        if c['mustSail']:
+            constraint_exp = [variables['y'][i, j, b] for b in range(boat_parameters['noBoats'])]
+            solver.Add(solver.Sum(constraint_exp) == 1)
+        else:
+            constraint_exp = [variables['x'][i, b] + variables['x'][j, b] for b in range(boat_parameters['noBoats'])]
+            solver.Add(solver.Sum(constraint_exp) <= 1)
+            
+        
+    
+    
+    
+        
+    
+    
+    
+    
         
     
 # In Python, you can also set the constraints as follows.
